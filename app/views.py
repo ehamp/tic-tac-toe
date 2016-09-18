17647a557ad40c0a0843b24189bc3012fa6412e1
Elizabeth Hamp <ehamp@seas.upenn.edu>
2016-09-18 16:49:16 -0400

    fixing ttt route

--- app/views.py
+++ app/views.py
@@ -7,18 +7,11 @@ def index():
     return 'Hello world!'
 
 
-@app.route("/boardstate", methods=['GET'])
+@app.route('/boardstate', methods=['GET'])
 def board():
     return 'hello its me board XXX 000 XXX'
 
 
 @app.route('/ttt', methods=['POST'])
 def inbound():
-    SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
-    if request.form.get('token') == SLACK_WEBHOOK_SECRET:
-        channel = request.form.get('channel_name')
-        username = request.form.get('user_name')
-        text = request.form.get('text')
-        inbound_message = username + " in " + channel + " says: " + text
-        print(inbound_message)
-    return Response(), 200
+    return "hi hi hi"
