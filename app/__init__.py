ad5ad449a9c910b28f5c7ac21f7f89a177e3048f
Elizabeth Hamp <ehamp@seas.upenn.edu>
2016-09-19 01:28:19 -0400

    fixing init

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from config import config
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object(app.config.from_object(config['development']))
+
 
 from app import views
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+db = SQLAlchemy()
+app = Flask(__name__)
+config_name='development'
+app.config.from_object(app.config.from_object(config[config_name]))
+config[config_name].init_app(app)
+# Set up extensions
+db.init_app(app)
